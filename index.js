3c62cab340cbb1fddfe8caf83b2406441db17bc2
Gustaf Gunér <gustaf@guner.se>
2018-11-13 12:09:49 +0100

    Added to scope and configured Prettier

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-let express = require("express");
-let request = require("request");
-let querystring = require("querystring");
-require("dotenv").config();
+let express = require('express');
+let request = require('request');
+let querystring = require('querystring');
+require('dotenv').config();
 
 let app = express();
 
 let redirect_uri =
-  process.env.SPOTIFY_REDIRECT_URI || "http://localhost:8888/callback";
+  process.env.SPOTIFY_REDIRECT_URI || 'http://localhost:8888/callback';
 
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
   res.redirect(
-    "https://accounts.spotify.com/authorize?" +
+    'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
-        response_type: "code",
+        response_type: 'code',
         client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: "user-read-private user-read-email",
-        redirect_uri
-      })
+        scope: 'user-read-private user-read-email user-read-playback-state',
+        redirect_uri,
+      }),
   );
 });
 
-app.get("/callback", (req, res) => {
+app.get('/callback', (req, res) => {
   let code = req.query.code || null;
   let authOptions = {
-    url: "https://accounts.spotify.com/api/token",
+    url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
       redirect_uri,
-      grant_type: "authorization_code"
+      grant_type: 'authorization_code',
     },
     headers: {
       Authorization:
-        "Basic " +
+        'Basic ' +
         new Buffer(
           process.env.SPOTIFY_CLIENT_ID +
-            ":" +
-            process.env.SPOTIFY_CLIENT_SECRET
-        ).toString("base64")
+            ':' +
+            process.env.SPOTIFY_CLIENT_SECRET,
+        ).toString('base64'),
     },
-    json: true
+    json: true,
   };
   request.post(authOptions, (error, response, body) => {
     var access_token = body.access_token;
-    let uri = process.env.FRONTEND_URI || "http://localhost:3000";
-    res.redirect(uri + "?access_token=" + access_token);
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000';
+    res.redirect(uri + '?access_token=' + access_token);
   });
 });
 
 let port = process.env.PORT || 8888;
+console.log('Server running on port ' + port);
 app.listen(port);
